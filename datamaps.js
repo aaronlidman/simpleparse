3358059a6ee0b5f48e575a62ffa150168e898d98
Aaron Lidman <aaronlidman@gmail.com>
2013-11-17 16:38:00 -0800

    Globby

--- datamaps.js
+++ datamaps.js
@@ -1,14 +1,33 @@
-// just the coordinates, for datamaps
+// all of the nodes
 var fs = require('fs'),
+    glob = require('glob'),
     readline = require('readline');
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
+var sGeo,
+    rl,
+    count = 0;
+
+glob('places_dump_*.geojson', {sync: true}, function (er, files) {
+    sGeo = files;
 });
 
-rl.on('line', function(line) {
+function eachLine(line) {
     line = JSON.parse(line).geometry.coordinates;
-    process.stdout.write(line[1] + ',' + line[0] + '\n');
-});
+    process.stdout.write(line[1].toFixed(6) + ',' + line[0].toFixed(6) + '\n');
+}
+
+function nextFile() {
+    var file = sGeo[count];
+    if (file) {
+        rl = readline.createInterface({
+            input: fs.createReadStream(file),
+            outstream: process.stdout,
+            terminal: false
+        });
+    }
+    count += 1;
+    rl.on('line', eachLine);
+    rl.on('close', nextFile);
+}
+
+nextFile();
--- simpler.js
+++ simpler.js
@@ -1,17 +1,36 @@
 // remove some unnecessary properties
 var fs = require('fs'),
+    glob = require('glob'),
     readline = require('readline');
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
+var sGeo,
+    rl,
+    count = 0;
+
+glob('places_dump_*.geojson', {sync: true}, function (er, files) {
+    sGeo = files;
 });
 
-rl.on('line', function(line) {
+function eachLine(line) {
     line = JSON.parse(line);
     delete line.id;
     delete line.properties.href;
     delete line.properties.owner;
     process.stdout.write(JSON.stringify(line) + '\n');
-});
+}
+
+function nextFile() {
+    var file = sGeo[count];
+    if (file) {
+        rl = readline.createInterface({
+            input: fs.createReadStream(file),
+            outstream: process.stdout,
+            terminal: false
+        });
+    }
+    count += 1;
+    rl.on('line', eachLine);
+    rl.on('close', nextFile);
+}
+
+nextFile();
